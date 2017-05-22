4f02295e79c9014fa0ecb0ed8f01ac0c8a5106b1
jesus-rodriguez1 <jesus.rodriguez@speeracademy.org>
2017-05-22 14:05:57 -0500

    v0.2.0.0 second commit
    Add new code

--- content.js
+++ content.js
@@ -9,3 +9,24 @@ chrome.runtime.onMessage.addListener(
     }
   }
 ); 
+
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('checkPage');
+  checkPageButton.addEventListener('click', function() {
+
+    chrome.tabs.getSelected(null, function(tab) {
+      d = document;
+
+      var f = d.createElement('form');
+      f.action = 'https://www.google.com/';
+      f.method = 'post';
+      var i = d.createElement('input');
+      i.type = 'hidden';
+      i.name = 'url';
+      i.value = tab.url;
+      f.appendChild(i);
+      d.body.appendChild(f);
+      f.submit();
+    });
+  }, false);
+}, false);
