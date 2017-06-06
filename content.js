edf6c427d2576070f92c20b9e41222774c6c913d
jesus-rodriguez1 <jesus.rodriguez@speeracademy.org>
2017-06-06 13:57:32 -0500

    v0.3.0.0 6th commit
    Able to add html or words into the website.

--- content.js
+++ content.js
@@ -1,32 +1,6 @@
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "clicked_browser_action" ) {
-      var firstHref = $("a[href^='http']").eq(0).attr("href");
-
-      console.log(firstHref);
-
-      chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
-    }
-  }
-); 
-
-document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
-
-    chrome.tabs.getSelected(null, function(tab) {
-      d = document;
-
-      var f = d.createElement('form');
-      f.action = 'https://www.google.com/';
-      f.method = 'post';
-      var i = d.createElement('input');
-      i.type = 'hidden';
-      i.name = 'url';
-      i.value = tab.url;
-      f.appendChild(i);
-      d.body.appendChild(f);
-      f.submit();
-    });
-  }, false);
-}, false);
+$(document).ready(function() {
+var $paragraph = $('<p>Do not check your grades and enjoy this spiderman gif</p>');
+$(document).ready(function() {
+    $('#signin-custom-message').after($paragraph);
+});
+    }); 
